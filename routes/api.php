c30bf52ab06540b3c6c3b3d9f239b8009e2e30c7
Murat Gorguner <gorguner@gmail.com>
2023-09-22 07:52:27 +0100

    Created the basic models and the auth functionality with endpoints.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Api\V1\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('auth/register', Auth\RegisterController::class);
+Route::post('auth/login', Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+    Route::put('password', Auth\PasswordUpdateController::class);
+    Route::post('auth/logout', Auth\LogoutController::class);
 });
